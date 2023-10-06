c14a06837f697f3d1476fc4d14a4d05b1ff4353b
anitkrjha <anitkjha31@gmail.com>
2023-10-06 11:05:39 +0530

    moved connection string to env file

--- database.ts
+++ database.ts
@@ -4,7 +4,7 @@ import { FastifyInstance } from 'fastify'
 
 async function dbConnector(app: FastifyInstance, options: any) {
     app.register(fastifyPostgres, {
-        connectionString: "postgresql://postgres:1h4gzcLLN2djzmfT@db.rjvcvchpforpyhebcaty.supabase.co:5432/postgres"
+        connectionString: process.env.DB_URL
     })
 }
 
